89f6026427bafc108fdf7f0564a7e2c28850bec6
czy0538 <czy0538@gmail.com>
2019-08-04 17:02:47 +0800

    2019年8月4日 17:01:45

---
+++ 0基础语法/src/Main.java
@@ -0,0 +1,82 @@
+//import java.util.Scanner;
+//
+//public class Main
+//{
+//	//	public static int[] reverse(int[] in)
+////	{
+////		int[] array = new int[in.length];
+////		for (int i = 0, j=in.length-1; i < in.length; ++i,--j)
+////		{
+////
+////				array[j] = in[i];
+////
+////		}
+////		return array;
+////	}
+////	public static int max(int... e)
+////	{
+////		int max = e[0];
+////		for (int i = 0; i < e.length; i++)
+////		{
+////			if (max < e[i])
+////			{
+////				max = e[i];
+////			}
+////		}
+////		return max;
+////	}
+//
+//
+//	public static void main(String[] args)
+//	{
+//		int x;
+//		A aa=new A();
+//		System.out.println(aa.i+x);
+////		int [] array={1, 2, 3, 4, 5};
+////		array=reverse(array);
+////		for (var i:array)
+////		{
+////			System.out.println(i);
+////		}
+//	}
+//}
+////class Circle
+////{
+////	final double pi=3.1415926;
+////	double r;
+////	Circle()
+////	{
+////		Scanner input=new Scanner(System.in);
+////		System.out.print("请输入半径r：");
+////		r=input.nextDouble();
+////	}
+////	Circle(double rr)
+////	{
+////		r=rr;
+////	}
+////	double area()
+////	{
+////		return Math.pow(r,2)*pi;
+////	}
+////}
+//class A
+//{
+//	int i;
+//}
+public class Main
+{
+	public static void main(String[] args)
+	{
+		A a = new A();
+	}
+}
+
+class A
+{
+	int i;
+
+	void setI(int i)
+	{
+		this.i = i;
+	}
+}
